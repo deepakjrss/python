b79168a5109c5ef2c5cddd037f30622ed8e07a2e
LAPTOP-NPM72ARP\HP <deepakjallepalli26@gmail.com>
2022-10-29 16:45:22 +0530

    frloop

---
+++ froloop.py
@@ -0,0 +1,16 @@
+#accept a number from the user
+#calculate and print the sum of all the numbers
+#from 1 to input number
+#using for loop
+
+
+n=int(input("enter a number"))
+sum=0
+for i in range(0,n+1):
+    sum=sum+i
+print("the sum is:",sum)
+
+
+
+
+
