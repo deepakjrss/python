dd2dce095bc689c64ad09f75775613160cbd3ca0
LAPTOP-NPM72ARP\HP <deepakjallepalli26@gmail.com>
2022-10-29 12:34:20 +0530

    practice

--- practice1.py
+++ practice1.py
@@ -1,3 +1,8 @@
+#Take user input (string)
+#if the length of the string is greater than 3
+#add "ing" as a suffix in the original string
+#else print the same string given by the user
+
 x=str(input(""))
 y=len(x)
 if y>3:
