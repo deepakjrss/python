0233e1fbadb19142776fbbeb99f3410855ea5813
LAPTOP-NPM72ARP\HP <deepakjallepalli26@gmail.com>
2022-12-01 18:22:49 +0530

    sets

---
+++ sets.py
@@ -0,0 +1,10 @@
+#store multiple values in single variable
+#unordered
+#unchangable or unmutable
+#duplicates are not allowed
+
+myset={"superbike","supercar","yacht"}
+print(myset)
+myset2={"car","bike","car","boat"}
+print(myset2)
+print(len(myset))                                                                                                                                
