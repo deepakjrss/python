eaae79c54a08b0b49e1f54481ffe4227d821691c
Deepak <deeepakjallepalli26@gmail.com>
2022-12-10 16:35:45 +0530

    laptop

---
+++ classesObj.py
@@ -0,0 +1,15 @@
+# class laptop:
+#     def config():
+#         print("asus","i7","1tb")
+
+# laptop1 = laptop()
+
+
+class Student:
+    def info(self):
+        print("nmae","class") 
+student1=Student ()
+students2=Student()
+student1.info()
+students2.info()
+  
